940d611276aa60c180900adb81a49b07c104c994
Zaynab RIFI <zaynab.rifi4@etu.univ-poitiers.fr>
2025-10-08 14:20:47 +0200

    :qaadd tournees relationship to Depot model
    add tournees relationship to Depot model

--- app/Models/Depots.php
+++ app/Models/Depots.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Depots extends Model
 {
     use HasFactory;
 
     protected $fillable = ['nom', 'adresse', 'capacite', 'code_postal', 'ville', 'longitude', 'latitude'];
-}
 
+    /**
+     * Relation many-to-many avec Tournee
+     */
+    public function tournees(): BelongsToMany
+    {
+        return $this->belongsToMany(Tournee::class, 'depot_tournee')
+                    ->withPivot('ordre')
+                    ->withTimestamps()
+                    ->orderByPivot('ordre');
+    }
+}
