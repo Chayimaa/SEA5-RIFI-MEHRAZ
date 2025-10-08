17e3f5ce96ea4aa6f2adad557929484551cefe48
Zaynab RIFI <zaynab.rifi4@etu.univ-poitiers.fr>
2025-10-08 14:18:02 +0200

    add Tournee model with depot relationship

--- app/Models/Tournee.php
+++ app/Models/Tournee.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tournee extends Model
 {
-    //
+    protected $fillable = [
+        'nom',
+        'date_tournee',
+        'heure_depart',
+        'statut',
+        'remarques'
+    ];
+
+    protected $casts = [
+        'date_tournee' => 'date',
+        'heure_depart' => 'datetime',
+    ];
+
+    // Relation many-to-many avec Depot
+    public function depots()
+    {
+        return $this->belongsToMany(Depot::class)
+                    ->withPivot('ordre')
+                    ->orderBy('ordre');
+    }
 }
